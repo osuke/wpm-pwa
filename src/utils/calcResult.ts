daeb1f2d23e9f32b8843b3d6df4f9680b1ee92b5
OsukeUesugi <funxion.mail@gmail.com>
2019-05-11 01:02:06 +0900

    modified calc logic to calc milliseconds

--- src/utils/calcResult.ts
+++ src/utils/calcResult.ts
@@ -1,7 +1,7 @@
 const calcResult = (sentences: string, seconds: number): number => {
   const num = sentences.split(' ').length;
 
-  return Math.floor(num / seconds * 60);
+  return Math.floor(num / Math.floor(seconds / 100) * 60);
 };
 
 export default calcResult;
--- tests/unit/calcResult.spec.ts
+++ tests/unit/calcResult.spec.ts
@@ -2,7 +2,7 @@ import calcResult from '../../src/utils/calcResult';
 
 describe('calc WPM', () => {
   it('calcResult returns correct result', () => {
-    const result = calcResult('Hello my name is awesome app. Nice to meet you.', 7);
+    const result = calcResult('Hello my name is awesome app. Nice to meet you.', 711);
     expect(result).toBe(85);
   });
 });
