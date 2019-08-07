d0fc0c55c501d8fecbd381e55932a3a7167fe740
OsukeUesugi <funxion.mail@gmail.com>
2019-08-07 22:04:46 +0900

    fixed bug not to show Infinity

--- src/utils/calcResult.ts
+++ src/utils/calcResult.ts
@@ -1,7 +1,8 @@
 const calcResult = (sentences: string, seconds: number): number => {
   const num = sentences.split(' ').length;
+  const sec = seconds < 100 ? 100 : seconds;
 
-  return Math.floor(num / Math.floor(seconds / 100) * 60);
+  return Math.floor(num / Math.floor(sec / 100) * 60);
 };
 
 export default calcResult;
